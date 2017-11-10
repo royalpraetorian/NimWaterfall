7d4d197f9a124f88c1d61d66628961587fb51bc9
ChrisEllis <CEllis@student.neumont.edu>
2017-11-10 15:47:48 -0700

    Blah

--- TestHarness/ChrisTest.cs
+++ TestHarness/ChrisTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TestHarness
 {
 	[TestClass]
-	public class UnitTest1
+	public class ChrisTest
 	{
 		[TestMethod]
 		public void TestMethod1()
